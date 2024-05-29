880f36b01db8611e8f9a08675f0a60fdbc562b2b
Luis David a Meza <luisdavid_204@hotmail.com>
2024-05-29 20:41:06 -0300

    iniciando os arquivos para o formulario

---
+++ Main.js
@@ -0,0 +1,7 @@
+/* crie um script em php capaz de capturar as informações destes tres campos presentes no formulario html utilizando o metodo post e que também insira estas informações em um banco de dados mysql com as seguintes configurações
+parametros de conexão: endereço:localhost
+usuario: root
+senha: sem senha
+Nome do banco: teste
+Nome da tabela: dados */
+
